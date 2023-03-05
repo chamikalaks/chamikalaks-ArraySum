98ccef02a70a7783006fbcee364e8b98a8b005c0
chamikalaks <chamikalaks@gmail.com>
2023-03-05 05:46:15 +0000

    array sum

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -7,7 +7,13 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
+        int sum= 0;
+        
+        for( int i = 0;i<arr.length;i++){
+           //if(arr[i]>sum)
+            sum += arr[i];
+        }
 
-        return 0;
+        return sum;
     }
 }
